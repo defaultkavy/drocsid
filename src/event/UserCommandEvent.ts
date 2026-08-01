b1666af0010b120f095dc6d9e36f7533ff821b58
defaultkavy <defaultkavy@gmail.com>
2026-08-01 14:05:50 +0800

    fix: `UserCommandEvent` generic type default value

--- src/event/UserCommandEvent.ts
+++ src/event/UserCommandEvent.ts
@@ -2,7 +2,7 @@ import type { APIUser, APIUserApplicationCommandGuildInteraction, APIUserApplica
 import { Discord } from "../..";
 import { CommandBaseEvent } from "./CommandBaseEvent";
 
-export class UserCommandEvent<I extends APIUserApplicationCommandInteraction> extends CommandBaseEvent<I> {
+export class UserCommandEvent<I extends APIUserApplicationCommandInteraction = APIUserApplicationCommandInteraction> extends CommandBaseEvent<I> {
     user: APIUser;
     constructor(client: Discord.Client, interaction: I) {
         super(client, interaction);
